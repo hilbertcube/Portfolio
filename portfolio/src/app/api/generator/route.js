65d56ddfb1530576382cd57a864eb8fa0efc87ff
Don D. Le <ledongduu@gmail.com>
2025-03-14 14:22:29 -0700

    changed model

--- portfolio/src/app/api/generator/route.js
+++ portfolio/src/app/api/generator/route.js
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server"
 export async function POST(req, res) {
 	try {
 		const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-		const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+		const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 		const data = await req.json();
 		const prompt = data.body;
 		const result = await model.generateContent(prompt);
--- portfolio/src/app/poetry-bot/page.js
+++ portfolio/src/app/poetry-bot/page.js
@@ -3,7 +3,7 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { useState, useEffect, useRef } from "react";
 
-const topics = ["Nature", "Integral Calculus", "Humans Society", "Nihilism", "God", "The Bible", "Love", "Existentialism", "Time", "The Universe", "Memory", "Death", "Hope", "Philosophy", "Technology", "Freedom", "Justice", "Chaos", "Humanity", "Art", "Music", "Emotion", "Dreams", "History", "Wisdom", "Loneliness", "Suffering", "Creation", "Spirituality", "Peace", "Conflict"];
+const topics = ["Dying Nature", "Humans Society", "Nihilism", "God", "The Bible", "Love", "Existentialism", "Time", "The Universe", "Memory", "Death", "Hope", "Philosophy", "Technology", "Freedom", "Justice", "Chaos", "Humanity", "Art", "Music", "Emotion", "Bad Dreams", "Brutal History", "Wisdom", "Loneliness", "Suffering", "Creation", "Spirituality", "Peace", "Conflict", "War"];
 
 
 function getRandomTopic() {
@@ -11,7 +11,6 @@ function getRandomTopic() {
   return topics[randomIndex];
 }
 
-const prompt1 = "Give me a short poem about " + getRandomTopic() + ", with at least 15 lines. Only generate the poem and its title, and NOTHING else.";
 const prompt2 = "Give me a poem from a poet that is no more than 25 lines. Follow this format: title, enclosed in double star **, the poem, the author's name and year at the end. Only follow these instructions, and nothing else.";
 
 // `Give me a short poem about ${newTopic}, with at least 15 lines. Only generate the poem and its title (enclosed in double star **), and NOTHING else.`
@@ -29,8 +28,8 @@ export default function Home() {
     setIsTyping(true);
 
     const newTopic = getRandomTopic();
-    console.log(newTopic)
-    const prompt = `Give me a short poem about ${newTopic}, with at least 15 lines. Only generate the poem and its title (enclosed in double star **), and NOTHING else. Show emotions.`;
+    console.log(newTopic);
+    const prompt = `Give me a short poem about ${newTopic}, with at least 15 lines. Only generate the poem and its title (enclosed in double star **), and NOTHING else. Show emotions and creativity. Remember the previous poems and try not to use the same words.`;
 
     // Clear any existing interval
     if (typingIntervalRef.current) {
@@ -103,9 +102,8 @@ export default function Home() {
   return (
     <div>
       <main className="flex min-h-screen flex-col items-center justify-start p-4 sm:p-10 bg-black">
-        <div className="z-10 max-w-xl w-full text-center cursor-pointer rounded border border-gray-800 text-white p-5 ms:text-base text-[14px]">
-          {/* <p className="whitespace-pre-wrap font-Jura">I am a bot integrated using the Gemini API</p> */}
-          <p className="whitespace-pre-wrap font-Jura">{displayedText}</p>
+        <div className="chat-box">
+          <p className="whitespace-pre-wrap font-mono">{displayedText}</p>
         </div>
         <div className="mt-4 inline-flex flex-row gap-2">
           <a href="./" className = "font-Jura rounded hover:bg-blue-600 p-2 bg-blue-700 text-white">Go Back</a>
