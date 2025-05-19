5ac85e49ef7b3a93922aa158012d9256af1e7c5d
Don D. Le <ledongduu@gmail.com>
2025-05-18 23:34:53 -0700

    updated github links

--- portfolio/src/app/data/projects.js
+++ portfolio/src/app/data/projects.js
@@ -13,7 +13,7 @@ export const projects = [
     description: 'Point cloud generation and plotting for ORB-SLAM3. This project is a combination of two repositories, including a command-line wrapper and a point cloud generator. It utilizes two versions of YOLO: version 4 for C++ and version 11 for Python.',
     time_frame: 'Feb. 2025 - Present',
     languages_and_tools: ['C','C++', 'Python', 'ORB SLAM3', 'YOLOv4', 'YOLOv11', 'Linux'],
-    link: 'https://github.com/Continuum3416/darknet-YOLOv4-cpp'
+    link: 'https://github.com/hilbertcube/darknet-YOLOv4-cpp'
   },
   {
     organization: 'OpenGL 3D Object Renderer' ,
@@ -21,7 +21,7 @@ export const projects = [
     description: '3D Object Renderer for common geometric shapes—including triangle, cube, sphere, and torus (donut) using OpenGL, a cross-platform, low-level API for rendering 2D and 3D vector graphics. Built with CMake, GLFW, GLAD, GLEW, GLM, and FreeGLUT.',
     time_frame: 'Feb. 2025 - Present',
     languages_and_tools: ['C++','GLFW', 'GLAD', 'GLEW' , 'GLW' , 'FreeGLUT', 'CMake', 'MSVC', 'vcpkg'],
-    link: 'https://github.com/Continuum3416/OpenGL-Object-Rendering'
+    link: 'https://github.com/hilbertcube/OpenGL-Object-Rendering'
   },
   {
     organization: '2D Physics Engine' ,
@@ -29,7 +29,7 @@ export const projects = [
     description: 'A collection of repositories to implement physics from scratch in C++, utilizing SFML for real‑time rendering and visualization. Implemented multiple numerical integration methods, including Euler (explicit/implicit), Verlet, and Runge‑Kutta (RK4), to accurately simulate motion; integrated rigid‑body collision detection for realistic interactions. Added a spatial grid for better optimization.',
     time_frame: 'Jan. 2025 - Present',
     languages_and_tools: ['C++','SFML', 'CMake', 'vcpkg', 'Numerical Methods'],
-    link: 'https://github.com/Continuum3416/Spatial-Grid-Partitioning'
+    link: 'https://github.com/hilbertcube/Spatial-Grid-Partitioning'
   },
   {
     organization: 'Webs-for-Clubs' ,
@@ -45,7 +45,7 @@ export const projects = [
     description: 'Blog website. Coded from scratch using HTML, CSS, and Javascript to display writings about engineering, physics, mathematics, computer science, and other fun stuff.',
     time_frame: 'Jul. 2024 - Present',
     languages_and_tools: ['Javascript', 'HTML', 'CSS', 'AOS', 'MathJax', 'Prism.js', 'Github Action', 'Github API'],
-    link: 'https://github.com/Continuum3416/neumanncondition'
+    link: 'https://github.com/hilbertcube/neumanncondition'
   },
   {
     organization: 'Chladni Patterns Generator' ,
@@ -53,6 +53,6 @@ export const projects = [
     description: 'A Python scientific computing project to search and generate an unlimited number of Chladni patterns on square and circular domains. The patterns are obtained by solving for eigenvalues of the two-dimensional wave equation through separation of variables  and elimination of the Bessel functions of the 2nd kind.',
     time_frame: 'Feb. 2024 - May. 2024',
     languages_and_tools: ['Python', 'PDE', 'Numpy', 'Scipy', 'Conda', 'Itertools'],
-    link: 'https://github.com/Continuum3416/Chladni-Patterns-Generator'
+    link: 'https://github.com/hilbertcube/Chladni-Patterns-Generator'
   },
 ];
