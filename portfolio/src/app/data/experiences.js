e97573145278c99de8e5fcd532b97eab4321cf08
Don D. Le <ledongduu@gmail.com>
2025-05-23 14:07:07 -0700

    updates

--- portfolio/src/app/data/experiences.js
+++ portfolio/src/app/data/experiences.js
@@ -3,7 +3,7 @@ export const exp = [
     organization: 'California Polytechnic State University' ,
     location: "Pomona, CA",
     job_title: 'Research Assistant, UAV Lab',
-    description: 'Developed UAV stereo camera trajectory estimation and 3D reconstruction system using ORB-SLAM3 and MASt3R-SLAM. Integrated ORB-SLAM3 with YOLOv8 in C++ for real-time UAV navigation. Improved runtime performance by 50% and achieved 90% detection accuracy with custom-labeled data.',
+    description: 'Developed UAV stereo camera trajectory estimation and 3D reconstruction system using ORB-SLAM3 and MASt3R-SLAM. Integrated ORB-SLAM3 concurrently with YOLOv8 in C++ for real-time UAV navigation. Improved runtime performance by 50% and achieved 90% detection accuracy with custom-labeled data.',
     time_frame: 'Sep. 2024 - Present',
     languages_and_tools: ['C++', 'Python', 'VMWare', 'ORB-SLAM3', 'Ubuntu', 'Gazebo', 'CMake', 'YOLOv4', 'PyTorch'],
   },
@@ -11,7 +11,7 @@ export const exp = [
     organization: 'California Polytechnic State University' ,
     location: "Pomona, CA",
     job_title: 'Computer Vision Research Intern',
-    description: 'High-performance UAV image processing using MPI (multi-core processing), OpenMP (multi-threading), and CUDA ORB/SURF with OpenCV C++. Achieved a 300% increase in the speed of the image stitching process and decreased the parallel-processing cost by 61% via CUDA.',
+    description: 'High-performance UAV image processing using MPI (multi-core processing), OpenMP (multi-threading), and CUDA ORB/SURF with OpenCV C++. Achieved a 300% increase in speed of feature-detection and image stitching processes and decreased the parallel-processing cost by 61% via CUDA.',
     highlight: 'Paper published at AIAA Scitech Forum 2025 (see paper below).',
     time_frame: 'Jun. 2024 - Sep. 2024',
     languages_and_tools: ['C++', 'Python','OpenCV', 'MPI', 'OpenMP', 'CUDA', 'CMake'],
@@ -25,3 +25,4 @@ export const exp = [
     languages_and_tools: ['Differential Equations', 'Linear Algebra', 'C++', 'Java', 'Calculus', 'Classical Mechanics'],
   },
 ];
+
