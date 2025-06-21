0fc67e322434e6a8e956ff0e3dfa370feb418063
Don D. Le <ledongduu@gmail.com>
2025-06-20 18:18:59 -0700

    updated gpa

--- portfolio/src/app/data/education.js
+++ portfolio/src/app/data/education.js
@@ -29,7 +29,7 @@ export const education = [
   {
     organization: 'Mt. San Antonio College' ,
     degree_and_major: 'Computer Engineering, Associate Degree in Mathematics',
-    gpa: "3.95/4.00",
+    gpa: "3.96/4.00",
     time: '2022 - 2025',
     relevant_coursework: ["Calculus I, II, III", "Engineering Physics I, II, III", "Differential Equations and Linear Algebra", "Java Programming and OOP", "C++ and Advanced C++ Programming", "x86 Assembly Language", "Data Structures and Algorithms", "Electrical Circuit Analysis", "Microcontrollers and Programming for Engineers", "Discrete Mathematics and Logic"],
     activities_and_awards: ["Vice President of Math Club", "Secretary of SMACS", "Putnam Mathematical Competition", "Math, C.S., and Physics Tutor", "Dean's Honor List (All semesters)", "AMATYC Math Competition", "Web-for-Clubs"]
